17f7fb76fb6fe9a3cca8c0ba01a338baaa45e3d8
Amanda Ng <amandang19@gmail.com>
2016-11-05 00:39:00 +0800

    Completed / optimised 3-opt - Also optimised 2-opt - Don't really need to optimise it further? With limit of 50 milliseconds each, total runtime is 0.09s

--- main.cpp
+++ main.cpp
@@ -15,6 +15,13 @@ static inline chrono::time_point<chrono::high_resolution_clock> now() {
     return chrono::high_resolution_clock::now();
 }
 
+void printTour(vector<int> tour) {
+    for (int i = 0; i < tour.size(); i++) {
+        printf("%d ", tour[i]);
+    }
+    printf("\n");
+}
+
 void printDistMatrix() {
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
@@ -36,6 +43,9 @@ void computeDistMatrix() {
     }
 }
 
+/*
+ Calculates tour from tour[0] to tour[N-1] and back to tour[0]
+*/
 int computeTourLength(vector<int> tour) {
     int totalDistance = 0;
     for (int i = 0; i < N - 1; i++) {
@@ -46,6 +56,9 @@ int computeTourLength(vector<int> tour) {
     return totalDistance;
 }
 
+/*
+ Algorithm taken from Kattis TSP brief
+*/
 vector<int> greedyTour() {
     int best;
     int curr;
@@ -75,12 +88,27 @@ vector<int> greedyTour() {
 
 vector<int> twoOptSwap(vector<int> tour, int i, int k) {
     vector<int> newTour;
-    newTour.insert(newTour.end(), tour.begin(), tour.begin() + i);
-    newTour.insert(newTour.end(), tour.rbegin() + (N - 1 - k), tour.rbegin() + (N - i));
-    newTour.insert(newTour.end(), tour.begin() + (k + 1), tour.end());
+    int numReverse = k - i + 1;
+    
+    // Essentially the same, just choosing to reverse a smaller set of tours
+    if (numReverse < int(N/2)) {
+        newTour.insert(newTour.end(), tour.begin(), tour.begin() + i);
+        newTour.insert(newTour.end(), tour.rbegin() + (N - 1 - k), tour.rbegin() + (N - i));
+        newTour.insert(newTour.end(), tour.begin() + (k + 1), tour.end());
+    }
+    
+    else {
+        newTour.insert(newTour.end(), tour.rbegin(), tour.rbegin() + (N - 1 - k));
+        newTour.insert(newTour.end(), tour.begin() + i, tour.begin() + (k + 1));
+        newTour.insert(newTour.end(), tour.rbegin() + (N - i), tour.rend());
+    }
+    
     return newTour;
 }
 
+/*
+ Simply reverse the tour in-between i and k (i & k inclusive)
+*/
 vector<int> twoOpt(vector<int> tour, chrono::time_point<chrono::high_resolution_clock> &deadline) {
     bool hasImprovement = true;
     int bestDistance = computeTourLength(tour);
@@ -88,11 +116,69 @@ vector<int> twoOpt(vector<int> tour, chrono::time_point<chrono::high_resolution_
         hasImprovement = false;
         for (int i = 0; i < N; i++) {
             for (int k = i + 1; k < N; k++) {
-                vector<int> newTour = twoOptSwap(tour, i, k);
-                int newDistance = computeTourLength(newTour);
+                
+                int iB = i - 1;
+                int kA = k + 1;
+                
+                if (i == 0) {
+                    iB = N-1;
+                }
+                
+                if (k == N - 1) {
+                    kA = 0;
+                }
+                
+                int removedEdges = bestDistance - dist[tour[i]][tour[iB]] - dist[tour[k]][tour[kA]];
+                int newDistance = removedEdges + dist[tour[iB]][tour[k]] + dist[tour[i]][tour[kA]];
+                
                 if (newDistance < bestDistance) {
-                    tour = newTour;
+                    tour = twoOptSwap(tour, i, k);
                     bestDistance = newDistance;
+                    hasImprovement = true;
+                }
+            }
+        }
+    }
+    return tour;
+}
+
+/* 
+ 2 possibilities for 3 OPT
+ Given 0 1 2 3 4 5 6 7 8 with i = 3 and j = 6:
+ - 1st permutation = 0 1 2 6 7 8 3 4 5
+ - 2nd permutation = 0 1 2 5 4 3 8 7 6
+*/
+vector<int> threeOpt(vector<int> tour, chrono::time_point<chrono::high_resolution_clock> &deadline) {
+    bool hasImprovement = true;
+    int bestDistance = computeTourLength(tour);
+    while (hasImprovement && chrono::high_resolution_clock::now() < deadline) {
+        hasImprovement = false;
+        for (int i = 1; i < N; i++) {
+            for (int j = i + 1; j < N; j++) {
+                
+                int removedEdges = bestDistance - dist[tour[i]][tour[i-1]] - dist[tour[j]][tour[j-1]] - dist[tour[N-1]][tour[0]];
+                int tour1Dist = removedEdges + dist[tour[i-1]][tour[j]] + dist[tour[N-1]][tour[i]] + dist[tour[j-1]][tour[0]];
+                int tour2Dist = removedEdges + dist[tour[i-1]][tour[j-1]] + dist[tour[N-1]][tour[i]] + dist[tour[j]][tour[0]];
+                
+                if (tour1Dist < tour2Dist && tour1Dist < bestDistance) {
+                    vector<int> newTour1;
+                    newTour1.insert(newTour1.end(), tour.begin(), tour.begin() + i);
+                    newTour1.insert(newTour1.end(), tour.begin() + j, tour.end());
+                    newTour1.insert(newTour1.end(), tour.begin() + i, tour.begin() + j);
+                    
+                    bestDistance = tour1Dist;
+                    tour = newTour1;
+                    hasImprovement = true;
+                }
+                
+                else if (tour2Dist < tour1Dist && tour2Dist < bestDistance) {
+                    vector<int> newTour2;
+                    newTour2.insert(newTour2.end(), tour.begin(), tour.begin() + i);
+                    newTour2.insert(newTour2.end(), tour.rbegin() + (N - j), tour.rbegin() + (N - i));
+                    newTour2.insert(newTour2.end(), tour.rbegin(), tour.rbegin() + (N - j));
+                    
+                    bestDistance = tour2Dist;
+                    tour = newTour2;
                     hasImprovement = true;
                 }
             }
@@ -118,8 +204,12 @@ int main () {
     tour = greedyTour();
     
     //auto start = now();
+    
     auto twoOptLimit = now() + chrono::milliseconds(50);
     tour = twoOpt(tour, twoOptLimit);
+    
+    auto threeOptLimit = now() + chrono::milliseconds(50);
+    tour = threeOpt(tour, threeOptLimit);
     
     //chrono::milliseconds totalTime = chrono::duration_cast<chrono::milliseconds>(now() - start);
     //cout << "Total time = " << totalTime.count() << "\n";
